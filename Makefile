3d7de882a5d17a897e1cc089deada2cc1f64e40a
JPGuillemin <root@hyperion.darkstar.com>
2026-04-17 11:37:14 +0200

    modernize vfilter

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 
 
 NAME = netpkg
-VERSION = 10.0
+VERSION = 11.5
 DESTDIR =
 LIBSUFFIX = 64
 PREFIX = /usr
@@ -21,78 +21,54 @@ SMALLICONPATH = $(DESTDIR)$(PREFIX)/share/icons/hicolor/16x16/apps
 CONFIGPATH = $(DESTDIR)/etc
 SOURCESPATH = $(DESTDIR)/etc/netpkg.d
 
-DEFINE = -D_REENTRANT 
+DEFINE = -D_REENTRANT
 
-all : vfilter listbuilder $(NAME).po
+all : vfilter
 
-vfilter : vfilter.o 
+vfilter : vfilter.o
 	gcc $(CFLAGS) $(DEFINE) -o $@ $^
 
-vfilter.o : vfilter.c 
-	gcc $(CFLAGS) $(DEFINE) -c $^
-	
-listbuilder : listbuilder.o 
-	gcc $(CFLAGS) $(DEFINE) -o $@ $^
-
-listbuilder.o : listbuilder.c 
+vfilter.o : vfilter.c
 	gcc $(CFLAGS) $(DEFINE) -c $^
 
 debug: DEFINE += -DDEBUG
 debug: CFLAGS += -g
-debug: vfilter listbuilder
+debug: vfilter
 
-
-netpkg.po: 
-	xgettext -s --from-code=UTF-8 --language=Shell -d netpkg -o netpkg.po zenpkg
-	sed -i 's/CHARSET/UTF-8/' netpkg.po
-	for PO in `ls po/*.po` ; do \
-		msgmerge -U -N --previous $$PO netpkg.po; \
-		LANGUAGE=`basename $$PO .po`; \
-		echo -n "$$LANGUAGE.mo : "; \
-		msgfmt -o mo/$$LANGUAGE.mo -v $$PO; \
-	done;	
-	rm -f po/*.po~
-
-libinstall : vfilter listbuilder 
-	strip vfilter	
-	strip listbuilder
+libinstall : vfilter
+	strip vfilter
 	install -D -m 0755 -o root -g root vfilter $(LIBEXECPATH)/vfilter
-	install -D -m 0755 -o root -g root listbuilder $(LIBEXECPATH)/listbuilder
 
-install : netpkg zenpkg netpkg.desktop libinstall $(NAME).po
+install : netpkg zenpkg netpkg.desktop libinstall
 	mkdir -p $(SMALLICONPATH) $(ICONPATH) $(DOCPATH) $(SOURCESPATH) $(DESKTOPPATH)
 	install -D -m 0755 -o root -g root netpkg $(SBINPATH)/netpkg
 	install -D -m 0755 -o root -g root zenpkg $(SBINPATH)/zenpkg
 	install -D -m 0755 -o root -g root netpkg.desktop $(DESKTOPPATH)/netpkg.desktop
+	install -D -m 0755 -o root -g root netpkg.8 $(MANPATH)/netpkg.8
 	install -D -m 644 -o root -g root netpkg.conf $(CONFIGPATH)/netpkg.conf
 	install -D -m 644 -o root -g root icons/scalable/*.svg $(ICONPATH)/
 	install -D -m 644 -o root -g root icons/16x16/*.png $(SMALLICONPATH)/
 	install -m 644 -o root -g root doc/* $(DOCPATH)
 	install -m 644 -o root -g root netpkg.d/* $(SOURCESPATH)
 	install -m 644 -o root -g root netpkg.d/.* $(SOURCESPATH)
-	for MO in `ls mo/*.mo` ; do \
-		LANGUAGE=`basename $$MO .mo`; \
-		mkdir -p $(LOCALEPATH)/$$LANGUAGE/LC_MESSAGES; \
-		install -D -m 644 -o root -g root $$MO $(LOCALEPATH)/$$LANGUAGE/LC_MESSAGES/netpkg.mo; \
-	done;
 	mkdir -p $(VARPATH)
 	install -D -m 644 -o root -g root DepsDB $(VARPATH)/DepsDB
 
 uninstall :
-	rm -f $(LIBEXECPATH)/vfilter $(LIBEXECPATH)/listbuilder
-	rm -f $(SBINPATH)/netpkg 
+	rm -f $(LIBEXECPATH)/vfilter
+	rm -f $(SBINPATH)/netpkg
 	rm -f $(SBINPATH)/zenpkg
 	rm -rf $(DOCPATH)
 	rm -rf $(SOURCESPATH)
 	rm -f $(CONFIGPATH)/netpkg.conf
-	rm -f $(LOCALEPATH)/*/LC_MESSAGES/$(NAME).mo
 	rm -f $(VARPATH)/DepsDB
-  
+	rm -f $(MANPATH)/netpkg.8
+
 clean :
-	rm -f vfilter.o vfilter listbuilder.o listbuilder netpkg.po po/*.po~ mo/*.mo $(NAME).po
+	rm -f vfilter.o vfilter
 
 
-	
+
 
 
 
--- vfilter.c
+++ vfilter.c
@@ -1,6 +1,7 @@
 /*
- * vfilter can understand version strings
+ * vfilter - compare two package names
  *
+ * Modified from vfilter
  * Copyright (C) 2006 Jean-Philippe Guillemin <jp.guillemin@free.fr> and Thibaud Guerin <guth@ploposor.com>
  *
  * This file is free software; the copyright holder gives unlimited
@@ -13,22 +14,14 @@
  * PARTICULAR PURPOSE.
  */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sysexits.h>
-#include <linux/types.h>
 #include <regex.h>
 
-
-#define VERSION "0.2"
+#define VERSION "1.0"
 #define TRUE 1
 #define FALSE 0
-#define EQUAL 0
-#define LESSER -1
-#define GREATER 1
 
 #ifdef DEBUG
 #define dbg(fmt,args...) fprintf(stderr, fmt , ## args)
@@ -38,19 +31,12 @@
 
 struct pkgfields
 {
-	char cat[129];
-	char name[257];
-	char softname[129];
-	char version[33];
-	char arch[33];
-	char build[33];
-	char vserial[129];
-	char bserial[65];
+  char name[257];
+  char version[33];
+  char build[33];
+  char vserial[129];
+  char bserial[65];
 };
-
-regex_t regex;
-regmatch_t pmatch[3];
-size_t size = 0;
 
 /**********************/
 /*      PROTOTYPES    */
@@ -63,329 +49,285 @@ int getfields (struct pkgfields *pkg);
 int vserialize (struct pkgfields *pkg);
 int bserialize (struct pkgfields *pkg);
 
-/* string version comparison function */
-int strverscmp(const char *s1, const char *s2);
-
 /* usage function */
 int usage(void);
-
 
 /***************** MAIN ********************/
 int main(int argc, char *argv[]) {
 
-	if (argc < 2) {
-		usage();
-		exit(EXIT_FAILURE);
-	}
+  if (argc != 3) {
+    usage();
+    exit(EXIT_FAILURE);
+  }
 
-	int i,c; 
-	char *filename = NULL;
-	char *query = NULL;
-	int showall = FALSE;
-	int showupdates = FALSE;
-	int showdowngrades = FALSE;
-	int shownews = FALSE;
-	FILE *fd;
+  struct pkgfields *pkg1;
+  pkg1 = (struct pkgfields *) malloc(sizeof(struct pkgfields));
+  struct pkgfields *pkg2;
+  pkg2 = (struct pkgfields *) malloc(sizeof(struct pkgfields));
 
-	struct pkgfields *pkg1;
-	pkg1=(struct pkgfields *) malloc (sizeof(struct pkgfields));
-	struct pkgfields *pkg2;
-	pkg2=(struct pkgfields *) malloc (sizeof(struct pkgfields));
-	char buffer[256];
-	int versioncmp,buildcmp;
+  int versioncmp, buildcmp;
+  regex_t regex;
 
-	while ((i = getopt(argc, argv, "f:q:audnh")) != EOF){
-		switch (i){
-			case 'f':
-				filename = optarg;
-				break;
-			case 'u':
-				showupdates = TRUE;
-				break;
-			case 'd':
-				showdowngrades = TRUE;
-				break;
-			case 'n':
-				shownews = TRUE;
-				break;
-			case 'a':
-				showall = TRUE;
-				break;
-			case 'q':
-				query = optarg;
-				break;
-			case 'h': usage(); exit(EXIT_FAILURE);
-		}
-	}
+  // Copy package names from arguments
+  strncpy(pkg1->name, argv[1], sizeof(pkg1->name) - 1);
+  pkg1->name[sizeof(pkg1->name) - 1] = '\0';
 
-	if (filename == NULL) {
-		fd = stdin;
-	}else{
-		if((fd=fopen(filename, "rb")) == 0) {
-			dbg("Error opening file %s !\n",filename);
-			exit(EXIT_FAILURE);
-		}
-	}
+  strncpy(pkg2->name, argv[2], sizeof(pkg2->name) - 1);
+  pkg2->name[sizeof(pkg2->name) - 1] = '\0';
 
-	if ( regcomp(&regex, "^.*-([^-]*)-[^-]*-([^-]*)\\.t[glx]z$", REG_EXTENDED )) {
-		dbg("getfields() : regex failed compilation\n");
-		return(EXIT_FAILURE);
-	}
+  // Compile regex for package name parsing
+  if (regcomp(&regex, "^.*-([^-]*)-[^-]*-([^-]*)\\.t[glx]z$", REG_EXTENDED)) {
+    fprintf(stderr, "Error: regex compilation failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	while (fgets (buffer, sizeof(buffer), fd) != NULL) {
-		buffer[strlen(buffer) - 1 ] = '\0';
-		// dbg("main() : buffer = %s\n", buffer);
+  // Extract version and build from package names
+  if (getfields(pkg1) != 0) {
+    fprintf(stderr, "Error: cannot process package name: %s\n", pkg1->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		memset(pkg1, 0, sizeof(struct pkgfields));
-		memset(pkg2, 0, sizeof(struct pkgfields));	
-		
-		c = sscanf (buffer, "%s |%s | %s",pkg1->cat ,pkg1->name , pkg2->name);
+  if (getfields(pkg2) != 0) {
+    fprintf(stderr, "Error: cannot process package name: %s\n", pkg2->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		if (showall == TRUE) {
-			printf("%s\n", buffer);
-			continue;
-		}
+  // Serialize versions for comparison
+  if (vserialize(pkg1) != 0) {
+    fprintf(stderr, "Error: serializing version for %s\n", pkg1->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		if (strlen(pkg1->name) == 0) continue;
+  if (vserialize(pkg2) != 0) {
+    fprintf(stderr, "Error: serializing version for %s\n", pkg2->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		if (query != NULL) {
-			if ( strncmp (query, pkg1->name, strlen(query) ) != 0){
-				continue;
-			}
-		}
+  // Compare versions
+  versioncmp = strcmp(pkg1->vserial, pkg2->vserial);
+  dbg("%s versions %s <-> %s\n", pkg1->name, pkg1->vserial, pkg2->vserial);
 
-		if ( getfields (pkg1) != 0 ) {
-			dbg("main() : can't process %s\n", pkg1->name);
-			continue;
-		}
+  if (versioncmp < 0) {
+    // version1 < version2
+    dbg("version1 < version2 (%d)\n", versioncmp);
+    regfree(&regex);
+    free(pkg1);
+    free(pkg2);
+    return 2;
+  } else if (versioncmp > 0) {
+    // version1 > version2
+    dbg("version1 > version2 (%d)\n", versioncmp);
+    regfree(&regex);
+    free(pkg1);
+    free(pkg2);
+    return 1;
+  }
 
-		if (shownews == TRUE) {
-			if (strlen(pkg2->name) == 0) printf("%s\n", buffer);
-			continue;
-		}
+  /* If we are here then version1 == version2,
+     so we need to look at the BUILD field */
 
-		if ( getfields (pkg2) != 0 ) {
-			dbg("main() : can't process %s\n", pkg2->name);
-			continue;
-		}
+  if (bserialize(pkg1) != 0) {
+    fprintf(stderr, "Error: serializing build for %s\n", pkg1->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		if ( vserialize (pkg1) != 0 ) {
-			dbg("serialize() error while processing %s\n", pkg1->name);
-		}
-		if ( vserialize (pkg2) != 0 ) {
-			dbg("serialize() error while processing %s\n", pkg1->name);
-		}
-		
-		versioncmp=strcmp(pkg1->vserial, pkg2->vserial);
-		dbg("%s versions %s <-> %s\n",pkg1->name, pkg1->vserial, pkg2->vserial);
+  if (bserialize(pkg2) != 0) {
+    fprintf(stderr, "Error: serializing build for %s\n", pkg2->name);
+    regfree(&regex);
+    exit(EXIT_FAILURE);
+  }
 
-		if ( versioncmp < 0){		// version1 < version2 <=> downgrade
-			if (showdowngrades == TRUE) printf("%s\n", buffer);
-			dbg("version1 < version2 (%d)\n", versioncmp);
-			continue;
-		}else if( versioncmp > 0){	// version1 > version2 <=> update
-			if (showupdates == TRUE) printf("%s\n", buffer);
-			dbg("version1 > version2 (%d)\n", versioncmp);
-			continue;
-		}
+  dbg("version1 == version2\n");
+  buildcmp = strcmp(pkg1->bserial, pkg2->bserial);
+  dbg("%s builds %s <-> %s\n", pkg1->name, pkg1->bserial, pkg2->bserial);
 
-		/* if we are here then version1 == version2, so we need to
-		take a look at the BUILD field */
+  regfree(&regex);
+  free(pkg1);
+  free(pkg2);
 
-		if ( bserialize (pkg1) != 0 ) {
-			dbg("serialize() error while processing %s\n", pkg2->name);
-		}
-		
-		if ( bserialize (pkg2) != 0 ) {
-			dbg("serialize() error while processing %s\n", pkg2->name);
-		}
+  if (buildcmp == 0) {
+    // build1 == build2 => same package
+    dbg("build1 == build2 (%d)\n", buildcmp);
+    return 0;
+  }
 
-		
-		dbg("version1 == version2\n");
-		buildcmp=strcmp(pkg1->bserial, pkg2->bserial);
-		dbg("%s builds %s <-> %s\n",pkg1->name, pkg1->bserial, pkg2->bserial);
-		
-		if ( buildcmp == 0){		// build1 == build2 <=> same package
-		    dbg("build1 == version2 (%d)\n", versioncmp);
-			continue;
-		}
-
-		if ( buildcmp < 0) {		// build1 < build2 <=> downgrade
-			if (showdowngrades == TRUE) printf("%s\n", buffer);
-			dbg("build1 < build2 (%d)\n", buildcmp);
-			continue;
-		}else{						// build1 > build2 <=> update
-			if (showupdates == TRUE) printf("%s\n", buffer);
-			dbg("build1 > build2 (%d)\n", buildcmp);
-			continue;
-		}
-
-	memset(buffer, 0, 256);
-	
-	}
-	fclose (fd);
-	regfree(&regex);
-	return(EXIT_SUCCESS);
+  if (buildcmp < 0) {
+    // build1 < build2
+    dbg("build1 < build2 (%d)\n", buildcmp);
+    return 2;
+  } else {
+    // build1 > build2
+    dbg("build1 > build2 (%d)\n", buildcmp);
+    return 1;
+  }
 }
 
 /* Get fields from package */
-int getfields (struct pkgfields *pkg) {
+int getfields(struct pkgfields *pkg) {
 
-	int start,end;
+  int start, end;
+  regex_t regex;
+  regmatch_t pmatch[3];
+  size_t size = 0;
 
+  if (regcomp(&regex, "^.*-([^-]*)-[^-]*-([^-]*)\\.t[glx]z$", REG_EXTENDED)) {
+    dbg("getfields() : regex failed compilation\n");
+    return EXIT_FAILURE;
+  }
 
-	if ( regexec(&regex, pkg->name, 3, pmatch, 0) ) {
-		dbg("getfields() : regex doesn't match\n");
-		return(EXIT_FAILURE);
-	}else{
+  if (regexec(&regex, pkg->name, 3, pmatch, 0)) {
+    dbg("getfields() : regex doesn't match\n");
+    regfree(&regex);
+    return EXIT_FAILURE;
+  } else {
+    // Retrieving the version
+    start = pmatch[1].rm_so;
+    end = pmatch[1].rm_eo;
+    size = end - start;
+    strncpy(pkg->version, pkg->name + start, size);
+    pkg->version[size] = '\0';
 
-		// Retrieving the version
-		start = pmatch[1].rm_so;
-		end = pmatch[1].rm_eo;
-		size = end - start;
-		strncpy (pkg->version, pkg->name + start, size);
-		pkg->version[size] = '\0';
+    dbg("getfields() : package version = %s\n", pkg->version);
 
-		dbg("getfields() : package version = %s\n", pkg->version);
+    // Retrieving the build
+    start = pmatch[2].rm_so;
+    end = pmatch[2].rm_eo;
+    size = end - start;
+    strncpy(pkg->build, pkg->name + start, size);
+    pkg->build[size] = '\0';
 
-		// Retrieving the build
-		start = pmatch[2].rm_so;
-		end = pmatch[2].rm_eo;
-		size = end - start;
-		strncpy (pkg->build, pkg->name + start, size);
-		pkg->build[size] = '\0';
+    dbg("getfields() : package build = %s\n", pkg->build);
+  }
 
-		dbg("getfields() : package build = %s\n", pkg->build);
-
-	}
-
-	return(EXIT_SUCCESS);
+  regfree(&regex);
+  return EXIT_SUCCESS;
 }
 
-
-char *strings_tab[]=
+char *strings_tab[] =
   { "cvs","svn","git","alpha1","alpha2","alpha3","alpha4","alpha",
-  	"beta1","beta2","beta3","beta4","beta","pre1","pre2","pre3","pre4",
-  	"pre","rc1","rc2","rc3","rc4","rc",
-  	"a","b","c","d","e","f","g","h","i","j","k","l",0 };
-char high_flags_tab[]="000111112222233333444446666777788880";
- char low_flags_tab[]="555123451234512345123451234123412340";
+    "beta1","beta2","beta3","beta4","beta","pre1","pre2","pre3","pre4",
+    "pre","rc1","rc2","rc3","rc4","rc",
+    "a","b","c","d","e","f","g","h","i","j","k","l",0 };
+char high_flags_tab[] = "000111112222233333444446666777788880";
+char low_flags_tab[] = "555123451234512345123451234123412340";
 
 /* Transform homosapiens into Hal9000 style version string */
 int vserialize (struct pkgfields *pkg) {
 
-	unsigned int i, dots, found, index;
-	char flag[2];
-	char *offset;
-	char buffer[9];
-	strncpy(flag,"55",2);
+  unsigned int i, dots, found, index;
+  char flag[2];
+  char *offset;
+  char buffer[9];
+  strncpy(flag,"55",2);
 
-	// Processing version *****************
+  // Processing version *****************
 
-	memset(pkg->vserial, '0', 128);
-	
-	// We remove leading '0'
-	offset = pkg->version;
-	while ((*offset) == '0') offset++;
-	
-	i=0; dots=0;
+  memset(pkg->vserial, '0', 128);
 
-	for (; *offset; offset++) {
-		
-		/* check numbers first as they are the most common char */
-		if (*offset >= '0' && *offset <= '9'){
-			buffer[i++] = *(offset);
-			continue;
-		}
-		
-		/* check "." or "_" */
-		if (*offset == '.' || *offset == '_'){
-			strncpy (pkg->vserial + (++dots * 8) - i, buffer, i);
-			i=0; 
-			continue;
-		}
-		
-		/* check "cvs", "svn", "git", "alpha", "beta", "pre" and "rc" ... */
-		found=FALSE;
-		for (index=0; strings_tab[index]; index++) {
-			if ( !strncasecmp(offset, strings_tab[index], strlen(strings_tab[index])) ){
-				flag[0] = high_flags_tab[index];
-				flag[1] = low_flags_tab[index];
-				offset += strlen(strings_tab[index]) - 1; // -1 as we offset++ before next global for loop
-				found=TRUE;
-				break;
-			}			
-		}
-		if ( found == FALSE ) buffer[i++] = *(offset);
-	}
-	
-	strncpy (pkg->vserial + (++dots * 8) - i, buffer, i);
-	
-	strncpy(pkg->vserial + 126, flag,2);
-	
-	pkg->vserial[128] = '\0';
-	
-	dbg("serialize() : vserial = %s\n", pkg->vserial);
+  // We remove leading '0'
+  offset = pkg->version;
+  while ((*offset) == '0') offset++;
 
+  i=0; dots=0;
 
-	return(EXIT_SUCCESS);
+  for (; *offset; offset++) {
+
+    /* check numbers first as they are the most common char */
+    if (*offset >= '0' && *offset <= '9'){
+      buffer[i++] = *(offset);
+      continue;
+    }
+
+    /* check "." or "_" */
+    if (*offset == '.' || *offset == '_'){
+      strncpy (pkg->vserial + (++dots * 8) - i, buffer, i);
+      i=0;
+      continue;
+    }
+
+    /* check "cvs", "svn", "git", "alpha", "beta", "pre" and "rc" ... */
+    found=FALSE;
+    for (index=0; strings_tab[index]; index++) {
+      if (!strncasecmp(offset, strings_tab[index], strlen(strings_tab[index]))) {
+        flag[0] = high_flags_tab[index];
+        flag[1] = low_flags_tab[index];
+        offset += strlen(strings_tab[index]) - 1; // -1 as we offset++ before next global for loop
+        found=TRUE;
+        break;
+      }
+    }
+    if (found == FALSE) buffer[i++] = *(offset);
+  }
+
+  strncpy (pkg->vserial + (++dots * 8) - i, buffer, i);
+
+  strncpy(pkg->vserial + 126, flag, 2);
+
+  pkg->vserial[128] = '\0';
+
+  dbg("serialize() : vserial = %s\n", pkg->vserial);
+
+  return 0;
 }
 
 /* Transform homosapiens into Hal9000 style build string */
 int bserialize (struct pkgfields *pkg) {
 
-	unsigned int i, dots;
-	char flag[2];
-	char *offset;
-	char buffer[9];
-	strncpy(flag,"55",2);
+  unsigned int i, dots;
+  char flag[2];
+  char *offset;
+  char buffer[9];
+  strncpy(flag,"55",2);
 
+  memset(pkg->bserial, '0', 64);
 
-	memset(pkg->bserial, '0', 64);
+  // We remove leading '0'
+  offset = pkg->build;
+  while ((*offset) == '0') offset++;
 
-	// We remove leading '0'
-	offset = pkg->build;
-	while ((*offset) == '0') offset++;
-	
-	i=0; dots=0;
+  i=0; dots=0;
 
-	for (; *offset; offset++) {
-		
-		/* check numbers first as they are the most common char */
-		if (*offset >= '0' && *offset <= '9'){
-			buffer[i++] = *(offset);
-			continue;
-		}
-		
-		/* check "." or "_" or "z" */
-		if (*offset == '.' || *offset == '_' || *offset == 'z'){
-			strncpy (pkg->bserial + (++dots * 8) - i, buffer, i);
-			i=0; 
-			continue;
-		}
-		
-		buffer[i++] = *(offset);
-	}
-	
-	strncpy (pkg->bserial + (++dots * 8) - i, buffer, i);
-	
-	pkg->bserial[64] = '\0';
+  for (; *offset; offset++) {
 
-	dbg("serialize() : bserial = %s\n", pkg->bserial);
+    /* check numbers first as they are the most common char */
+    if (*offset >= '0' && *offset <= '9'){
+      buffer[i++] = *(offset);
+      continue;
+    }
 
-	return(EXIT_SUCCESS);
+    /* check "." or "_" or "z" */
+    if (*offset == '.' || *offset == '_' || *offset == 'z'){
+      strncpy (pkg->bserial + (++dots * 8) - i, buffer, i);
+      i=0;
+      continue;
+    }
+
+    buffer[i++] = *(offset);
+  }
+
+  strncpy (pkg->bserial + (++dots * 8) - i, buffer, i);
+
+  pkg->bserial[64] = '\0';
+
+  dbg("serialize() : bserial = %s\n", pkg->bserial);
+
+  return EXIT_SUCCESS;
 }
 
 /* usage */
 int usage(void) {
-	printf("\nvfilter v%s : compare versions and returns updated packages\n", VERSION);
-	printf("Usage : vfilter [-a] [-u] [-d] [-n] -q query -f filename\n");
-	printf("[-a] : show all\n");
-	printf("[-u] : show updates\n");
-	printf("[-d] : show downgrades\n");
-	printf("[-n] : show news\n");
-	printf("\"filename\" is in format \"package1 | package2\"\n");
-	printf("\"query\" is a filter matching on package1\n");
-	printf("no \"filename\" <=> stdin is used\n");
-	return(EXIT_SUCCESS);
+  printf("\nvfilter v%s : compare two package names\n", VERSION);
+  printf("Usage: vfilter <package1> <package2>\n\n");
+  printf("Compares package versions and builds extracted from package names.\n");
+  printf("Package format: name-version-arch-build.t[glx]z\n\n");
+  printf("Returns:\n");
+  printf("  1  if package1 > package2\n");
+  printf("  0  if package1 == package2\n");
+  printf("  2  if package1 < package2\n");
+  return EXIT_SUCCESS;
 }
